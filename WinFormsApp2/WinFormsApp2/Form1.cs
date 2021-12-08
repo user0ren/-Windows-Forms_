0effe03655b7396b03d974b0dc4047bfdf5ddcd5
user0ren <88614567+user0ren@users.noreply.github.com>
2021-12-08 16:40:35 +0300

    Add files via upload

---
+++ WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привет");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 newForm1 = new Form1();
+            newForm1.Show();
+
+            Form2 newForm2 = new Form2(newForm1);
+            newForm2.Show();
+        }
+    }
+}
---
+++ WinFormsApp2/WinFormsApp2/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form2(Form1 f)
+        {
+            InitializeComponent();
+            f.BackColor = Color.Yellow;
+        }
+
+
+    }
+}
